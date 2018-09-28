827514f3515c13b0b22dc90fa6b3efba4b257839
wyzw <wyzw@wyzwdeMacBook-Pro.local>
2018-09-28 09:51:55 +0800

    first commit

---
+++ GetImageSize/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetImageSize
+//
+//  Created by wyzw on 2018/9/28.
+//  Copyright © 2018年 wyzw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GetImageSize/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetImageSize
+//
+//  Created by wyzw on 2018/9/28.
+//  Copyright © 2018年 wyzw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
